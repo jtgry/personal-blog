988cf323641f9bd2a33891a0eca60660f98a82ac
Tyler Gray <tgray.it@gmail.com>
2020-04-21 00:47:01 -0500

    Update-Bug-Fixes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,22 @@
-  // Image Process
-  imagesLoaded( document.querySelector('#content'), function( instance ) {
-    console.log('all images are loaded');
 
-    var myLazyLoad = new LazyLoad({
-      elements_selector: ".lazyload",
-      load_delay: 300
-    });
+var themeButton = document.querySelector(".theme-button");
+var root = document.getElementById('root');
+
+themeButton.addEventListener("click", function() {
+  root.classList.toggle("theme-dark");
+
+  
+
+});
+
+
+
+// Image Process
+imagesLoaded( document.querySelector('#content'), function( instance ) {
+  console.log('all images are loaded');
+
+  var myLazyLoad = new LazyLoad({
+    elements_selector: ".lazyload",
+    load_delay: 300
   });
+});
